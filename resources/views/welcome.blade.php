d3c492f0532f9092a594420c5dc5cb58ee62c1b0
milos01 <milosa942@gmail.com>
2016-11-09 19:33:05 +0100

    Title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>MILOSANDRIC</title>
 
         <!-- Fonts -->
         <link href="http://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet" type="text/css">
